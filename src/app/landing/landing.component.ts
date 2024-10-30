a3687ca5d51d7f40d856212ad09e2276629f3eeb
Krishna <appilivamsikrishna@gmail.com>
2024-10-31 05:15:59 +0530

    re3

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.css"],
 })
 export class LandingComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // Method to navigate to the upload component
+  navigateToUpload(): void {
+    this.router.navigate(["/upload"]); // Adjust '/upload' based on your routing path
   }
-
 }
