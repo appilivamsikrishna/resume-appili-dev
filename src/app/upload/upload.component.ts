36d16f9f12b59346c2b5307a886492ee2bcf10c1
Krishna <appilivamsikrishna@gmail.com>
2024-10-31 01:29:36 +0530

    upload and view working

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"; // Import necessary storage functions
-import { initializeApp } from "firebase/app";
-import { environment } from "../../environments/environment"; // Ensure your Firebase config is correct
 
 @Component({
   selector: "app-upload",
@@ -9,36 +7,42 @@ import { environment } from "../../environments/environment"; // Ensure your Fir
   styleUrls: ["./upload.component.css"],
 })
 export class UploadComponent implements OnInit {
+  selectedFile: File | null = null; // To store the selected file
   downloadURL: string; // Store the URL after upload
 
   constructor() {}
 
-  ngOnInit(): void {
-    // Initialize Firebase app
-    // initializeApp(environment.firebase); // Ensure Firebase is initialized with your configuration
+  ngOnInit(): void {}
+
+  // Method to handle file selection
+  fileSelected(event: any): void {
+    this.selectedFile = event.target.files[0]; // Store the selected file
   }
 
   // Method to handle file upload
-  uploadFile(event: any): void {
-    const file = event.target.files[0]; // Get the selected file
+  uploadFile(): void {
+    if (this.selectedFile) {
+      const storage = getStorage(); // Use the new getStorage function
+      const storageRef = ref(storage, this.selectedFile.name); // Create a reference to 'file.name'
 
-    // Initialize Firebase Storage and create a reference to the file
-    const storage = getStorage(); // Use the new getStorage function
-    const storageRef = ref(storage, file.name); // Create a reference to 'file.name'
+      // Upload the file
+      uploadBytes(storageRef, this.selectedFile)
+        .then((snapshot) => {
+          console.log("Uploaded a file!");
 
-    // Upload the file
-    uploadBytes(storageRef, file)
-      .then((snapshot) => {
-        console.log("Uploaded a file!");
+          // Get the download URL
+          getDownloadURL(storageRef).then((url) => {
+            this.downloadURL = url; // Store and use this URL to display or link to the file
 
-        // Get the download URL
-        getDownloadURL(storageRef).then((url) => {
-          this.downloadURL = url; // Store and use this URL to display or link to the file
-          console.log("File available at:", this.downloadURL);
+
+            console.log("File available at:", this.downloadURL);
+          });
+        })
+        .catch((error) => {
+          console.error("Error uploading file:", error);
         });
-      })
-      .catch((error) => {
-        console.error("Error uploading file:", error);
-      });
+    } else {
+      console.log("No file selected");
+    }
   }
 }
