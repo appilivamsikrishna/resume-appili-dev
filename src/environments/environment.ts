f535568c401e6b64582eccacaf4c04ab03049dd2
Krishna <appilivamsikrishna@gmail.com>
2024-10-31 00:45:12 +0530

    added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,37 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDhIAeuuYocqF1aNso4E679iAfBHGYtYE4",
+    authDomain: "cdn-appili-dev.firebaseapp.com",
+    projectId: "cdn-appili-dev",
+    storageBucket: "cdn-appili-dev.appspot.com",
+    messagingSenderId: "503552505927",
+    appId: "1:503552505927:web:0d9d6cf07c5cbc47755962",
+    measurementId: "G-9LK8N0NR2K",
+  },
 };
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDhIAeuuYocqF1aNso4E679iAfBHGYtYE4",
+//   authDomain: "cdn-appili-dev.firebaseapp.com",
+//   projectId: "cdn-appili-dev",
+//   storageBucket: "cdn-appili-dev.appspot.com",
+//   messagingSenderId: "503552505927",
+//   appId: "1:503552505927:web:0d9d6cf07c5cbc47755962",
+//   measurementId: "G-9LK8N0NR2K"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
