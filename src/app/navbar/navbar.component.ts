86f1f2eb47c4167fd2c5e6af236e35bdf72bef86
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2026-05-05 16:14:54 +0530

    fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppConstants } from '../app.constants';
 
+declare const bootstrap: any;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -14,6 +16,18 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  closeSidebarOnMobile(): void {
+    if (window.innerWidth >= 768) {
+      return;
+    }
+    const sidebarEl = document.getElementById('sidebarMenu');
+    if (!sidebarEl) {
+      return;
+    }
+    const collapse = bootstrap.Collapse.getOrCreateInstance(sidebarEl, { toggle: false });
+    collapse.hide();
   }
 
 }
